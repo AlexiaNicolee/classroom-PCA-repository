4ea0f3609436862b3aaa4bca1540b2d5efdd104e
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-15 16:37:55 -0400

    Add files via upload

---
+++ 20221215_003.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2 ;
+    printf("\n Conversao de metros para centimetros");
+
+    printf("\n Entre com numero 1 :");
+    scanf ("%d",&num1 );
+
+    num2 = num1 *100 ;
+    printf("\n Transformando o num 1 = %d metros em num 2 = %d centimetros ",num1,num2);
+    // conversao de metros para centimetros
+
+    return 0;
+}
