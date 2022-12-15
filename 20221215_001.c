d57e4a3c498156d4fc1b0c4b48b8381345f8bd50
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-15 14:41:16 -0400

    Add files via upload

---
+++ 20221215_001.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2 ,soma;
+    printf("\n soma entre dois numeros");
+
+    printf("\n Entre com o numero 1 :");
+    scanf ("%d",&num1 );
+
+    printf ("\n Entre com o numero 2:");
+    scanf ("%d",&num2 );
+
+    soma = num1 + num2;
+    printf("\n A soma entre o numero %d e o numero %d eh %d", num1, num2, soma);
+    // a soma entre o numero 2 e o numero 3 e 5
+
+    return 0;
+}
