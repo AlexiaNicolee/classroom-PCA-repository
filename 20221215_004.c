e87be5af3e6e1dc7113c61c673cc916d9351c2bb
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-19 12:39:18 -0400

    Add files via upload

---
+++ 20221215_004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    void main()
+{
+    float raio, area ;
+    printf("\n Area de um circulo");
+    printf("\n Entre com o raio :");
+    scanf ("%f",&raio );
+
+    area = ((raio*raio)*3.1415);
+    printf("\n A area do circulo eh %f ", area);
+    // Calcular a area do circulo
+
+    return 0;
+}
