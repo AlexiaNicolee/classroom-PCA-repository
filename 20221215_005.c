b472aa3e5f29bc6f6556e96dc5c9d34021aba16a
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-19 12:57:02 -0400

    Add files via upload

---
+++ 20221215_005.c
@@ -0,0 +1,16 @@
+#include <stdio.h>*/
+#include <stdlib.h>
+
+int main()
+{
+    float lado,area,area2;
+
+    printf ("Area de um quadrado \n Insira o valor do lado a: \n ");
+
+    scanf("%f", &lado);
+    area = (lado*lado);
+    area2 = (2*area);
+    printf("A area eh %f e o dobro da area eh %f \n",area,area2);
+
+    return 0;
+}
