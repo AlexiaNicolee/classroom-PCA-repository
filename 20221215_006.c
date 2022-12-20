37553782b9ab644eb72586675f59c35a6cb7ba06
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-19 21:32:02 -0400

    Update 20221215_006.c

--- 20221215_006.c
+++ 20221215_006.c
@@ -6,18 +6,18 @@ void main()
     float valorhora,horasmes,salario;
     char mes;
 
-    printf(" Quanto voce ganha por hora: \n");
+    printf(" Calculando o seu salario \n Informe quanto voce ganha por hora: \n");
     scanf ("%f", &valorhora);
 
     printf(" Numero de horas trabalhadas no mes: \n");
     scanf ("%f", &horasmes);
 
-    printf(" Qual mes: \n");
+    printf(" Qual mes (ex:janeiro) : \n");
     scanf ("%f", &mes);
 
     salario = (horasmes*valorhora);
 
     printf("\n Seu salario eh %.2f \n",salario);
-
+    
     return 0;
 }
