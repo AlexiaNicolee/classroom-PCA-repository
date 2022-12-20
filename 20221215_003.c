16d37ca7306322bcfa12e3a89b5a3363f197020a
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-19 21:21:50 -0400

    Update 20221215_003.c

--- 20221215_003.c
+++ 20221215_003.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    int num1,num2 ;
+    float num1,num2 ;
     printf("\n Conversao de metros para centimetros");
 
     printf("\n Entre com numero 1 :");
-    scanf ("%d",&num1 );
+    scanf ("%f",&num1 );
 
     num2 = num1 *100 ;
-    printf("\n Transformando o num 1 = %d metros em num 2 = %d centimetros ",num1,num2);
+    printf("\n Transformando o num 1 que eh %.2f metros em = %.2f centimetros ",num1,num2);
     // conversao de metros para centimetros
 
     return 0;
