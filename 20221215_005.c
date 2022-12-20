0b65e85f75441433b8955d155a8a51ecedd5f8e2
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-19 21:27:52 -0400

    Update 20221215_005.c

--- 20221215_005.c
+++ 20221215_005.c
@@ -5,13 +5,13 @@ int main()
 {
     float lado,area,area2;
 
-    printf ("Area de um quadrado \n Insira o valor do lado a: \n ");
+    printf ("Calculando a area de um quadrado \n Insira o valor do lado a: ");
     scanf("%f", &lado);
-    
+
     area = (lado*lado);
     area2 = (2*area);
-    
-    printf("A area eh %f e o dobro da area eh %f \n",area,area2);
+
+    printf(" A area eh %f e o dobro da area eh %f \n",area,area2);
 
     return 0;
 }
