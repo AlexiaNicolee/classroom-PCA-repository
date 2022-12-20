41f0c10a376124b89e3afe87432dc0885d6a887c
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-19 21:44:18 -0400

    Update Exercício 03.c

--- Exercício 03.c
+++ Exercício 03.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() 
+void main()
 {
-    float totalConta, PorcentagemDoGarcom;
+    float totalConta,totalDoGarcom, PorcentagemDoGarcom;
 
-    printf("Exercicio 03 - Forneça o valor da conta e a porcentagem dada ao garçom(A porcentagem deve ser em valor decimal) \n");
+    printf("Exercicio 03 - Forneca o valor da conta e a porcentagem dada ao garcom(A porcentagem deve ser em valor decimal) \n");
     scanf("%f %f", &totalConta, &PorcentagemDoGarcom);
 
-    float totalDoGarcom = totalConta *  (PorcentagemDoGarcom/ 100 );
+    totalDoGarcom = totalConta *  (PorcentagemDoGarcom/ 100 );
     printf("O garcom precisa receber R$%.2f", totalDoGarcom);
 
     return 0;
 }
+
