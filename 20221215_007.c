76918367615344256080e08e84387d92a7ae3fbf
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-19 21:36:18 -0400

    Update 20221215_007.c

--- 20221215_007.c
+++ 20221215_007.c
@@ -5,12 +5,12 @@ void main()
 {
     float C,F;
 
-    printf(" Transformando a temperatura de fahreinheit para celsius \n Informe a temperatura em Fahreinheit: \n");
+    printf(" Transformando a temperatura de Fahrenheit para celsius \n Informe a temperatura em Fahrenheit: \n");
     scanf ("%f", &F);
 
     C = (5 * (F-32) / 9);
 
-    printf("\n A temperatura em Celsius eh %.2f e em Fahreinheit eh %.2f \n" ,C,F);
+    printf("\n A temperatura em Celsius eh %.2f e em Fahrenheit eh %.2f \n" ,C,F);
 
     return 0;
 }
