c9e54509d66f654b182308822bd14f88565599f4
AlexiaNicolee <120610296+AlexiaNicolee@users.noreply.github.com>
2022-12-20 16:17:02 -0400

    Update 20222220002.c

--- 20222220002.c
+++ 20222220002.c
@@ -3,13 +3,19 @@
 
 int main()
 {
-    int i, soma = 0;
+    int i,num1, soma = 0;
 
-    printf("A soma de 50 numeros inteiros e soma dos numeros impares \n\n");
+    printf("A soma de 50 numeros inteiros e a soma dos numeros impares \n\n");
 
-    for(i =1; i <= 50; i++)
-        if(i % 2 == 1)
-            soma += i;
+    for(i =1; i <= 50; i++){
+        scanf("%d",&num1);
+
+        if(num1 % 2 != 0){
+            soma += num1;
+        }
+    }
 
     printf("Soma dos impares de 1 a 50: %d\n\n", soma);
+
+    return 0;
 }
